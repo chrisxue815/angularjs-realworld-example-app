61effdd8694950945ba8daca683ea1dacd9a0cb5
Chris Xue <chrisxue815@gmail.com>
2019-09-02 15:43:32 +0100

    Change API URL

--- src/js/config/app.constants.js
+++ src/js/config/app.constants.js
@@ -1,5 +1,5 @@
 const AppConstants = {
-  api: 'https://conduit.productionready.io/api',
+  api: 'https://3zgug3s5bg.execute-api.eu-west-1.amazonaws.com/dev',
   // api: 'http://localhost:3000/api',
   jwtKey: 'jwtToken',
   appName: 'Conduit',
